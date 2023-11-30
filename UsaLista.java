ab4ea67526eec60689e3cf534b8763eaf1d1bd34
BryanTuz <bryantuz05@gmail.com>
2023-11-29 23:41:52 -0500

    Update UsaLista.java

--- UsaLista.java
+++ UsaLista.java
@@ -17,18 +17,5 @@ public class UsaLista
 		lista.insertaAntesPrimerNodo("H");
 		System.out.println(lista);
 		lista.insertaAlFinal("Z");
-		System.out.println(lista);
-		lista.insertaEntreNodos("T", "R");
-		System.out.println(lista);
-		lista.insertaAntesPrimerNodo("K");
-		System.out.println(lista);
-		lista.insertaAlFinal("Ñ");
-		System.out.println(lista);
-		lista.borrarPrimerNodo();
-		System.out.println(lista);
-		lista.borrarUltimoNodo();
-		System.out.println(lista);
-		lista.borrarCualquierNodo("R");
-		System.out.println(lista);
     }
 }
