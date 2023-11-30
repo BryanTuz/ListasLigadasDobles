31fc88f4f65f8af687f7cabf97c1bd6ec9d6e321
BryanTuz <148660453+BryanTuz@users.noreply.github.com>
2023-11-29 23:19:07 -0500

    Add files via upload

---
+++ Node.java
@@ -0,0 +1,16 @@
+
+/**
+ * Write a description of class Nodes here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+
+//clase autorreferenciada
+
+public class Node {
+	String name; //campo de datos
+	Node previous; //campo enlace
+    Node next;   //campo enlace
+}
+
